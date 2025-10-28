f7dd76e172596421607c699b4374455ab0690b82
gersonraulb-sketch <gersonraulb@gmail.com>
2025-10-28 17:33:47 -0500

    Add files via upload

---
+++ ejerciclo.js
@@ -0,0 +1,41 @@
+//estudiantes
+const estudiantes = ["gerson","esteban","david","vocero","maicol","miguel","erwin"];
+
+while(estudiantes.length > 0){
+    console.log(estudiantes);
+
+    const betados = estudiantes.shift();
+    console.log(estudiantes);
+}
+
+//componentes
+const componentes = ["televisor","teclado","mouse"];
+
+console.log(componentes);
+
+componentes.push("usb","celular","audifonos");
+
+console.log(componentes);
+    
+let tel = [3001234567, 3122345678, 3113456789, 3204567890, 3175678901, 3146789012];
+
+for (let i = 0 ; i < tel.length; i++){
+    console.log(tel[i])
+}
+
+//animales
+let hervivoro = ["conejo","venado","vaca","elefante","panda"];
+
+hervivoro.forEach(i=>{
+    console.log(i);
+})
+
+
+//novias
+let novia = ["valentina","yurani","sofia"];
+
+for (let novias of novia){
+    console.log(`novias: ${novias}`)
+}
+
+
